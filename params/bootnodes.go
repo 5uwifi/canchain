f509cf147870775207da59e1ab8494ba6846d390
hsienfu <hsienfu.tseng@gmail.com>
2018-07-05 15:43:07 +0800

    update bootnodes

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -7,6 +7,10 @@ package params
 
 var MainnetBootnodes = []string{}
 
-var TestnetBootnodes = []string{}
+var TestnetBootnodes = []string{
+	"ccnode://ff8b923781aa6d11305dcb851f9ca89b874cc24a8bba07477b831ff43449a837c8c80b5713b7b469a4ca41ee48e0f462206be13063f287c3bd19bf8ac1b788d6@39.104.66.16:24242",
+}
 
-var DiscoveryV5Bootnodes = []string{}
+var DiscoveryV5Bootnodes = []string{
+	"ccnode://d88c07cb763f6b87e660b8d63f1322d719bb1bdb658c98f937311af2878ba662bd5d4a9a7873151b7be89eeaa4c16995580ae4a26f12c28e98dc20c4cf7856f5@39.104.66.16:24243",
+}
